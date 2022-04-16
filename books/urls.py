e822b1e8035ea5717283527b36d769bb6fcb49c8
KeremAydin98 <aydinke16@itu.edu.tr>
2022-04-16 14:52:22 +0300

    almost

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import BookListView, BookDetailView, SearchResultsView
+from .views import BookListView, BookDetailView, SearchResultsView, BookCreateView, BookUpdateView, BookDeleteView
 
 urlpatterns = [
     path('', BookListView.as_view(), name='book_list'),
+    path('new/', BookCreateView.as_view(), name='book_new'),
     path('<uuid:pk>', BookDetailView.as_view(), name='book_detail'),
+    path('<uuid:pk>/update/', BookUpdateView.as_view(), name='book_update'),
+    path('<uuid:pk>/update/delete/', BookDeleteView.as_view(), name='book_delete'),
     path('search/',SearchResultsView.as_view(), name='search_results'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,7 +1,10 @@
 from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Book
+from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 from django.db.models import Q
+
 
 class BookListView(LoginRequiredMixin, ListView):
 
@@ -10,13 +13,42 @@ class BookListView(LoginRequiredMixin, ListView):
     template_name = 'books/book_list.html'
     login_url = 'account_login'
 
-class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
+
+class BookDetailView(LoginRequiredMixin, DetailView):
 
     model = Book
     context_object_name = 'book'
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
-    permission_required = 'books.special_status'
+    #permission_required = 'books.special_status'
+
+
+class BookCreateView(LoginRequiredMixin, CreateView):
+
+    model = Book
+    context_object_name = 'book'
+    template_name = 'books/book_new.html'
+    login_url = 'account_login'
+    fields = ('title','author','price','cover',)
+
+
+class BookUpdateView(LoginRequiredMixin, UpdateView):
+
+    model = Book
+    context_object_name = 'book'
+    template_name = 'books/book_update.html'
+    login_url = 'account_login'
+    fields = ('title','author','price','cover',)
+
+
+class BookDeleteView(LoginRequiredMixin, DeleteView):
+
+    model = Book
+    template_name = 'books/book_delete.html'
+    login_url = 'account_login'
+    success_url = reverse_lazy('home')
+
+
 
 class SearchResultsView(ListView):
 
